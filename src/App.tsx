94914bd1189246c1ebc2fc5fc1a8bc83f650929f
abdullah-fswd <ansarimdabdullah794@gmail.com>
2025-10-31 00:03:37 +0530

    dagre layout and node positioning

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,42 @@ function App() {
   }
 }`;
 
+  const dagreGraph = new dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}));
+
+  const nodeWidth = 172;
+  const nodeHeight = 36;
+
+  function getLayoutedElements(nodes: Node[], edges: Edge[], direction = 'TB') {
+    const isHorizontal = direction === 'LR';
+    dagreGraph.setGraph({ rankdir: direction });
+
+    nodes.forEach((node) => {
+      dagreGraph.setNode(node.id, { width: nodeWidth, height: nodeHeight });
+    });
+
+    edges.forEach((edge) => {
+      dagreGraph.setEdge(edge.source, edge.target);
+    });
+
+    dagre.layout(dagreGraph);
+
+    const newNodes = nodes.map((node) => {
+      const nodeWithPosition = dagreGraph.node(node.id);
+      const newNode = {
+        ...node,
+        targetPosition: isHorizontal ? 'left' : 'top',
+        sourcePosition: isHorizontal ? 'right' : 'bottom',
+        position: {
+          x: nodeWithPosition.x - nodeWidth / 2,
+          y: nodeWithPosition.y - nodeHeight / 2,
+        },
+      };
+      return newNode;
+    });
+
+    return { nodes: newNodes, edges };
+  }
+
   function convertJsonToNodeAndEdges(jsonValue: any) {
     const nodes: NodeType[] = [];
     const edges: EdgeType[] = [];
@@ -134,7 +170,9 @@ function App() {
 
     convertJsonData(jsonValue, null, 'root', '$', 0);
 
-    return { nodes, edges };
+    const layouted = getLayoutedElements(nodes, edges, 'TB');
+
+    return layouted;
   }
 
   function handleSubmit() {
